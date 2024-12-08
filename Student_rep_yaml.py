88bc9b67581b08983d615c127cbf477f25185cc3
PolinaBY <112944644+PolinaBY@users.noreply.github.com>
2024-12-08 23:54:22 +0300

    Лаб2 Зад3. Объединить класс в общую иерархию с помощью наследования
    С помощью наследования объединить два написанных класса в общую иерархию

--- Student_rep_yaml.py
+++ Student_rep_yaml.py
@@ -1,7 +1,7 @@
 import yaml
 
 
-class MyEntityRepYaml(MyEntityRepBase):
+class StudentRepYaml(StudentStrategy):
     """Стратегия для работы с YAML."""
 
     def load(self, file_path: str) -> List[dict]:
@@ -14,3 +14,13 @@ class MyEntityRepYaml(MyEntityRepBase):
     def save(self, file_path: str, data: List[dict]):
         with open(file_path, 'w') as file:
             yaml.safe_dump(data, file, default_flow_style=False)
+
+    def add(self, student):
+        data = self.load()
+        data.append(student.to_dict())
+        self.save(data)
+
+    def display(self):
+        data = self.load(file_path)
+        for item in data:
+            print(item)
