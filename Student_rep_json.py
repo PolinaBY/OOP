af5c9234c4635212e3525034ec1c022ae5a56273
PolinaBY <112944644+PolinaBY@users.noreply.github.com>
2024-12-08 23:42:38 +0300

    Лаб2 Зад3 С помощью наследования объединить два написанных класса в общую иерархию
    С помощью наследования объединить два написанных класса в общую иерархию

--- Student_rep_json.py
+++ Student_rep_json.py
@@ -1,6 +1,7 @@
 import json
+import os
 
-class MyEntityRepJson(MyEntityRepBase):
+class StudentRepJson(StudentStrategy):
       """Стратегия для работы с JSON."""
 
     def load(self, file_path: str) -> List[dict]:
@@ -10,6 +11,12 @@ class MyEntityRepJson(MyEntityRepBase):
         except FileNotFoundError:
             return []
 
-    def save(self, file_path: str, data: List[dict]):
+    def write(self, file_path: str, data: List[dict]):
         with open(file_path, 'w') as file:
             json.dump(data, file, indent=4)
+
+   def display(self):
+       try:
+            data = self.load(file_path)
+            for item in data:
+               print(item)
