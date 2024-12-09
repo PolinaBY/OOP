d86ea7b7bbf1dd7b14dfc2f0c4df3788994ec73a
PolinaBY <112944644+PolinaBY@users.noreply.github.com>
2024-12-09 03:14:33 +0300

    Лаб2 Зад5 Выделите с помощью делегации и паттерна одиночка отдельный класс для работы с выбранной БД
    Выделите с помощью делегации и паттерна одиночка отдельный класс для работы с выбранной БД

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -1,15 +1,23 @@
 import psycopg2
+from psycopg2.extras import DictCursor
 
 class DBConnection:
     """Класс для работы с базой данных (Одиночка)."""
     
     _instance = None
     
-    def __new__(cls, connection_string: str):
+    def __new__(cls, host, user, password, database, port=3306):
         """Гарантирует, что будет создан только один экземпляр соединения с базой данных."""
         if cls._instance is None:
             cls._instance = super(DBConnection, cls).__new__(cls)
-            cls._instance._connection = psycopg2.connect(connection_string)
+            cls._instance._connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                dbname=database,
+                port=port,
+                cursor_factory=DictCursor
+            )
         return cls._instance
     
     def get_connection(self):
