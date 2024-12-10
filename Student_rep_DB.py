b68ae653cb548f786750542f2a7cd99932c5ec90
PolinaBY <112944644+PolinaBY@users.noreply.github.com>
2024-12-11 02:17:02 +0300

    Лаб2 Зад4 Добавлено отлавление  ошибки при повторяющихся записях

--- Student_rep_DB.py
+++ Student_rep_DB.py
@@ -4,7 +4,7 @@ class StudentRepDB:
     def __init__(self, host, user, password, database, port=3306):
         self.db_connection = DBConnection(host, user, password, database, port)
     
-    def get_by_id(self, student_id: int) -> dict:
+    def get_by_id(self, student_id: int) -> Student:
         """Получить объект по ID."""
         conn = self.db_connection.get_connection()
         with conn.cursor() as cursor:
@@ -12,35 +12,49 @@ class StudentRepDB:
             student = cursor.fetchone()
             if not student:
                 raise ValueError(f"Студент с ID {student_id} не найден.")
-            return dict(student)
+            return Student(
+                    student_id=student['student_id'],
+                    first_name=student['first_name'],
+                    last_name=student['last_name'],
+                    patronymic=student['patronymic'],
+                    phone=student['phone']
+            )
 
     
-    def get_k_n_short_list(self, k: int, n: int):
+    def get_k_n_short_list(self, k: int, n: int) -> List[Student]:
         """Получить список k по счету n объектов """
         offset = (n - 1) * k
         conn = self.db_connection.get_connection()
         with conn.cursor() as cursor:
             cursor.execute("SELECT * FROM students ORDER BY id LIMIT %s OFFSET %s", (k, offset))
-            return [dict(row) for row in cursor.fetchall()]
+            student = cursor.fetchone()
+            return [
+                Student(
+                    student_id=student['student_id'],
+                    first_name=student['first_name'],
+                    last_name=student['last_name'],
+                    patronymic=student['patronymic'],
+                    phone=student['phone']
+                ) for row in student
+            ]
     
     def add_student(self, student: Student):
         """Добавить объект в базу данных (сгенерировать новый ID)."""
         conn = self.db_connection.get_connection()
-        unique_fields = ("phone")
-        with conn.cursor() as cursor:
-            cursor.execute(
-                "SELECT COUNT(*) FROM students WHERE phone = %s",
-                (student['phone'])
-            )
-        if cursor.fetchone()[0] > 0:
-            raise ValueError("Студент с такими данными уже существует.")
-        cursor.execute(
-            "INSERT INTO students (first_name, last_name, patronymic, phone) VALUES (%s, %s, %s, %s) RETURNING id",
-            (student['first_name'], student['last_name'], student['patronymic'], student['phone'])
-        )
-        student_id = cursor.fetchone()[0]
-        conn.commit()
-        return student_id
+        try:
+            query = """
+                INSERT INTO students (first_name, last_name, patronymic, phone) VALUES (%s, %s, %s, %s)
+            """
+            values = (student.first_name, student.last_name, student.patronymic, student.phone)
+            with conn.cursor() as cursor:
+                cursor.execute(query, values)
+                conn.commit()
+
+        except MySQLError as e:
+            if e.args[0] == 1062:
+                raise ValueError(f"Студент с номероом {student.phone} уже существует.")
+            else:
+                raise Exception("При добавлении студента произошла непредвиденная ошибка.")
     
     def replace_by_id(self, student_id: int, new_student: dict):
         """Заменить элемент списка по ID."""
