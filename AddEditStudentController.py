98fa8b8006882d17cd0c1271eee8e153332a4a7c
PolinaBY <112944644+PolinaBY@users.noreply.github.com>
2024-12-16 18:31:17 +0300

    Update AddEditStudentController.py

--- AddEditStudentController.py
+++ AddEditStudentController.py
@@ -29,9 +29,16 @@ class AddEditStudentController:
             if self.student:
                 self.repository_adapter.update_by_id(self.student.student_id, updated_student)
             else:  # Создание нового
-                self.repository_adapter.add(updated_product)
+                self.repository_adapter.add(updated_student)
             self.view.destroy()  # Закрываем окно после успешного сохранения
         except (ValueError, InvalidOperation) as e:
             self.view.show_error(f"Validation error: {str(e)}")
         except Exception as e:
             self.view.show_error(f"Error saving student: {str(e)}")
+
+class AddStudentController:
+    def __init__(self,view: AddEditStudentWindow, repository_adapter: StudentAdapter, student=None)
+        self.view = view
+        self.repository_adapter = repository_adapter
+        self.student = student
+    def 
