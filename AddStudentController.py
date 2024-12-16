1c921f922915b1e1019e43e89de7296fc12e43eb
PolinaBY <112944644+PolinaBY@users.noreply.github.com>
2024-12-16 21:51:13 +0300

    Лаб3 Зад1. Обновлен контроллер для добавления новой записи

---
+++ AddStudentController.py
@@ -0,0 +1,28 @@
+import Adapter
+from AddStudentWindow import AddStudentWindow
+from Student import Student
+from decimal import Decimal, InvalidOperation
+
+
+class AddStudentController:
+    """Контроллер для добавления нового студента."""
+
+    def __init__(self, view: AddStudentWindow, repository_adapter: StudentAdapter):
+        self.view = view
+        self.repository_adapter = repository_adapter
+        self.view.set_save_command(self.handle_save)
+
+    def handle_save(self, data):
+        try:
+            new_student = Student.create_new_student(
+                first_name=data['first_name_entry'],
+                last_name=data['last_name_entry'],
+                patronymic=data['patronymic_entry'],
+                phone=data['phone_entry']
+            )
+            self.repository_adapter.add(new_student)
+            self.view.destroy()  # Закрываем окно после успешного добавления
+        except (ValueError, InvalidOperation) as e:
+            self.view.show_error(f"Ошибка валидации: {str(e)}")
+        except Exception as e:
+            self.view.show_error(f"Ошибка сохранения студента: {str(e)}")
